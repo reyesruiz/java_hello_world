004d15b36341b3d52797b854f53a7c515d6514ea
Reyes Ruiz <ruiz.reyes@yahoo.com>
2015-11-09 22:04:24 -0800

    Hello World Test

---
+++ java_hello_world/src/java_hello_world/Hello_World.java
@@ -0,0 +1,11 @@
+package java_hello_world;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("This is a test");
+		System.out.println("YEA YEA");
+	}
+
+}
